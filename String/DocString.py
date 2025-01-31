7550773ebe06146ae4bcfb02b661055d3c81a247
Asim Khan <asimkhan106988@gmail.com>
2025-01-31 18:44:04 +0530

    Doc Strint

---
+++ String/DocString.py
@@ -0,0 +1,9 @@
+def square(n):
+    # Python not take a as comment 
+    # Python take as Doc String
+    '''Take in a number n, returns the square'''
+    print(n**2)
+
+square(5)
+
+print(square.__doc__) #Doc String print 
