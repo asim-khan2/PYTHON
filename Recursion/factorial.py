8eb2ea0833912d3e8b3e907fc57a6d5ff0c3a085
Asim Khan <asimkhan106988@gmail.com>
2025-02-01 08:22:21 +0530

    Calculate factorial

---
+++ Recursion/factorial.py
@@ -0,0 +1,11 @@
+# function for calculating factorial 
+def factorial(n):
+    if(n==0 or n==1):
+        return 1
+    return n*factorial(n-1) # return result
+
+n = int(input("Enter number for calculate Factorial : "))
+
+res = factorial(n)
+
+print(f"{n} factorial is {res}")
