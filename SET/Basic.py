b2fa18d43e696c86f2f876432f125a3a68cbab42
Asim Khan <asimkhan106988@gmail.com>
2025-02-05 22:25:19 +0530

    Basic of set

--- SET/Basic.py
+++ SET/Basic.py
@@ -2,5 +2,12 @@ my_set = {1, 5, 6, 1, 34,} #set does't take duplicate item
 print(my_set)
 print(type(my_set))
 
-for i in my_set:
+for i in my_set: # for loop access the element of set
     print(i)
+
+s = {"Asim", 34, 3.5, 3432}
+print(s)
+
+#empty set 
+new_set = set() # that is empty set 
+print(type(new_set))
