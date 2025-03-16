dad8b6693a2b992a6edd834d276cd9ab9bdba8c7
Asim Khan <asimkhan106988@gmail.com>
2025-03-16 20:04:40 +0530

    Sum Function

---
+++ Function/sum.py
@@ -0,0 +1,7 @@
+def sum(a, b):
+    return a+b
+num1 = int(input("Enter number first : "))
+num2 = int(input("Enter number second : "))
+
+#function call
+print(f"{num1} + {num2} = {sum(num1, num2)}")
