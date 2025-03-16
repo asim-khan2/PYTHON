f9f7e01389e1bb81acce5611864f2b14562593d7
Asim Khan <asimkhan106988@gmail.com>
2025-03-16 20:28:59 +0530

    Average of 3 number

---
+++ Function/average_of_3No.py
@@ -0,0 +1,8 @@
+def average(a,b,c):
+    return (a+b+c)/3
+
+n1 = int(input("Enter frist number : "))
+n2 = int(input("Enter second number : "))
+n3 = int(input("Enter third number : "))
+
+print("Aveage of three number is : ", average(n1,n2,n3))
